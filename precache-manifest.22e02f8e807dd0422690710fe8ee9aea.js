d8dca9d33b98d7ebfe50cedb8cfc6431c1b888e7
Your Name~ <you@example.com>
2022-09-23 12:18:01 +0300

    Updates

---
+++ precache-manifest.22e02f8e807dd0422690710fe8ee9aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cf612f6ab78d6cde40f",
+    "url": "/react-counter/static/css/main.f1cf36d9.chunk.css"
+  },
+  {
+    "revision": "7cf612f6ab78d6cde40f",
+    "url": "/react-counter/static/js/main.7cf612f6.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-counter/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/react-counter/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "5cb7474a6d5e3f9c494f",
+    "url": "/react-counter/static/js/runtime~main.5cb7474a.js"
+  },
+  {
+    "revision": "f62a4cf0e71448b7c0fb96be0794d0e0",
+    "url": "/react-counter/index.html"
+  }
+];
